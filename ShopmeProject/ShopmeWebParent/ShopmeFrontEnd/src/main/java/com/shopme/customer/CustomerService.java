0e38daa959ef0ddf134b51579baa993c76ae59a2
minknhom <91058535+minknhom@users.noreply.github.com>
2023-07-25 00:46:04 +0700

    fb + gg oauth2 + customer detail

--- ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/customer/CustomerService.java
+++ ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/customer/CustomerService.java
@@ -7,52 +7,116 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import net.bytebuddy.utility.RandomString;
 
+import com.shopme.common.entity.AuthenticationType;
 import com.shopme.common.entity.Customer;
 
-@Transactional
+import net.bytebuddy.utility.RandomString;
+
 @Service
+@Transactional
 public class CustomerService {
 
+	@Autowired private CustomerRepository customerRepo;
+	@Autowired PasswordEncoder passwordEncoder;
+	
+	public boolean isEmailUnique(String email) {
+		Customer customer = customerRepo.findByEmail(email);
+		return customer == null;
+	}
+	
+	public void registerCustomer(Customer customer) {
+		encodePassword(customer);
+		customer.setEnabled(false);
+		customer.setCreatedTime(new Date());
+		customer.setAuthenticationType(AuthenticationType.DATABASE);
+		
+		String randomCode = RandomString.make(64);
+		customer.setVerificationCode(randomCode);
+		
+		customerRepo.save(customer);
+		
+	}
+	
+	public Customer getCustomerByEmail(String email) {
+		return customerRepo.findByEmail(email);
+	}
 
-    @Autowired private CustomerRepository customerRepo;
-
-    @Autowired PasswordEncoder passwordEncoder;
+	private void encodePassword(Customer customer) {
+		String encodedPassword = passwordEncoder.encode(customer.getPassword());
+		customer.setPassword(encodedPassword);
+	}
+	
+	public boolean verify(String verificationCode) {
+		Customer customer = customerRepo.findByVerificationCode(verificationCode);
+		
+		if (customer == null || customer.isEnabled()) {
+			return false;
+		} else {
+			customerRepo.enable(customer.getId());
+			return true;
+		}
+	}
+	
+	public void updateAuthenticationType(Customer customer, AuthenticationType type) {
+		if (!customer.getAuthenticationType().equals(type)) {
+			customerRepo.updateAuthenticationType(customer.getId(), type);
+		}
+	}
+	
+	public void addNewCustomerUponOAuthLogin(String name, String email,
+			AuthenticationType authenticationType) {
+		Customer customer = new Customer();
+		customer.setEmail(email);
+		setName(name, customer);
+		
+		customer.setEnabled(true);
+		customer.setCreatedTime(new Date());
+		customer.setAuthenticationType(authenticationType);
+		customer.setPassword("");
+		customer.setAddressLine1("");
+		customer.setCity("");
+		customer.setState("");
+		customer.setPhoneNumber("");
+		customer.setPostalCode("");
+		customer.setCountry("");
+		
+		customerRepo.save(customer);
+	}	
+	
+	private void setName(String name, Customer customer) {
+		String[] nameArray = name.split(" ");
+		if (nameArray.length < 2) {
+			customer.setFirstName(name);
+			customer.setLastName("");
+		} else {
+			String firstName = nameArray[0];
+			customer.setFirstName(firstName);
+			
+			String lastName = name.replaceFirst(firstName + " ", "");
+			customer.setLastName(lastName);
+		}
+	}
     
-    
-    public boolean isEmailUnique(String email) {
-        Customer customer = customerRepo.findByEmail(email);
-        return customer == null;
-    }
-    
-    
-    public void registerCustomer(Customer customer) {
-        encodePassword(customer);
-        customer.setEnabled(false);
-        customer.setCreatedTime(new Date());
+    public void update(Customer customerInForm) {
+        Customer customerInDB = customerRepo.findById(customerInForm.getId()).get();
         
-        String randomCode = RandomString.make(64);
-        customer.setVerificationCode(randomCode);
-        
-        customerRepo.save(customer);
-        
-    }
-
-    private void encodePassword(Customer customer) {
-        String encodedPassword = passwordEncoder.encode(customer.getPassword());
-        customer.setPassword(encodedPassword);
-    }
-    
-    
-    public boolean verify(String verificationCode) {
-        Customer customer = customerRepo.findByVerificationCode(verificationCode);
-        
-        if (customer == null || customer.isEnabled()) {
-            return false;
+        if (customerInDB.getAuthenticationType().equals(AuthenticationType.DATABASE)) {
+            if (!customerInForm.getPassword().isEmpty()) {
+                String encodedPassword = passwordEncoder.encode(customerInForm.getPassword());
+                customerInForm.setPassword(encodedPassword);            
+            } else {
+                customerInForm.setPassword(customerInDB.getPassword());
+            }        
         } else {
-            customerRepo.enable(customer.getId());
-            return true;
+            customerInForm.setPassword(customerInDB.getPassword());
         }
-    }
+        
+        customerInForm.setEnabled(customerInDB.isEnabled());
+        customerInForm.setCreatedTime(customerInDB.getCreatedTime());
+        customerInForm.setVerificationCode(customerInDB.getVerificationCode());
+        customerInForm.setAuthenticationType(customerInDB.getAuthenticationType());
+        
+        customerRepo.save(customerInForm);
+    }    
 }
